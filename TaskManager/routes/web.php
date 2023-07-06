1ccabcec56b4e53390abd5e4d7a3934a1ed022cd
Vitor Garcia <vitorh_mariano06@hotmail.com>
2023-07-06 14:02:32 -0300

    Arquivos do trabalho

---
+++ TaskManager/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Site\SiteController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/supports', [SupportController::class, 'index']) ->name ('support.index');    
+
+Route::get('/contato', [SiteController::class, 'contact']);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
